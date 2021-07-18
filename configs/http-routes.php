4e5841ba38b0104810efcc30300d5f9ca7ea19de
Kenny <kenny@coccoc.com>
2021-07-18 18:40:04 +0700

    Update path in route

--- configs/http-routes.php
+++ configs/http-routes.php
@@ -6,9 +6,7 @@ use Slim\App;
 use Slim\Http\Response;
 
 /** @var App $app */
-$app->group('/api/v1/users', function () use ($app) {
-    $app->post('[/]', ['Api\V1\UserController', 'add']);
-});
+$app->post('/api/v1/users[/]', ['Api\V1\UserController', 'add']);
 
 $app->get('/', function (Response $response) use ($app) {
     return $response->withJson('ok');
